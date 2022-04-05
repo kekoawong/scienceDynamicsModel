9d7187f6577dac605b1c3738e7667026e252f335
kekoawong <kwong6@nd.edu>
2022-04-05 15:22:27 -0400

    Empty max error

--- ScholarNetwork.py
+++ ScholarNetwork.py
@@ -1,10 +1,26 @@
 import networkx as nx
 import random
+from statistics import mode
 
 '''
 Inherited Graph class from networkx with methods used for scholar evolution
 '''
 class Graph(nx.Graph):
+
+    def determinePaperTopic(self, authors):
+        '''
+        Returns the main topicID, looping through the author data
+        '''
+        topics = {}
+        for author in authors:
+            print(self.nodes[author]["data"])
+            for top, papers in self.nodes[author]["data"].items():
+                if top not in topics:
+                    topics[top] = 0
+                topics[top] += len(papers)
+
+        # returns the topic with the most combined papers from all the authors
+        return max(topics, key=topics.get)
     
     def biasedRandomWalk(self, authors, probStop):
         '''
@@ -17,7 +33,8 @@ class Graph(nx.Graph):
 
         # base condition: stop at node if probStop hit or there are no new neighbors to traverse
         if random.random() < probStop or len(newNeighbors) == 0:
-            return authors
+            topic = self.determinePaperTopic(authors)
+            return topic, authors
         
         # create list representing probabilities for the neighboring nodes of the current coauthor
         probs = []
