e829aa13201275d66efdc764cdb724fa1f58d513
kekoawong <kwong6@nd.edu>
2022-04-06 15:55:35 -0400

    Added evolution function

--- evolution.py
+++ evolution.py
@@ -22,19 +22,58 @@ class Evolution:
 
         '''Inital Parameters'''
         self.newAuthor = 0
+        self.initialPaper = 0
 
         '''Initialize network with one author, one paper, and one topic'''
-        initialPaper = 0
         initialTopic = 0
-        self.network.add_node(self.newAuthor, data={initialTopic: [initialPaper]})
-        self.papers[initialPaper] = (initialTopic, [self.newAuthor])
-        self.topics[initialTopic] = [initialPaper]
+        self.network.add_node(self.newAuthor, data={initialTopic: [self.initialPaper]})
+        self.papers[self.initialPaper] = (initialTopic, [self.newAuthor])
+        self.topics[initialTopic] = [self.initialPaper]
+        self.initialPaper += 1
 
     def evolve(self, timeSteps=25):
         '''
         Function will continue evolution for the inputted timesteps
         '''
-        pass
+        for newPaper in range(self.initialPaper, timeSteps):
+
+            # Randomly select author from network, will be used as first author or first coauthor
+            currNodes = list(self.network.nodes())
+            authors = [random.choice(currNodes)]
+
+            # with probability, add new author to network set as main author with the coauthor
+            if random.random() < self.probNewAuthor:
+                # generate new author, add as the first author
+                self.newAuthor += 1
+                authors.insert(0, self.newAuthor)
+                # add node without data, disciplines will be added after paper is completed
+                self.network.add_node(self.newAuthor, data={})
+                self.network.add_edge(self.newAuthor, authors[1], weight=1, width=1)
+
+            # Add new paper, calling function
+            paper = self.network.biasedRandomWalk(authors, self.probStop, newPaper)
+            # print(f'Paper: {paper}')
+            self.papers[newPaper] = paper
+
+            # add paper to corresponding topic
+            if paper[0] not in self.topics:
+                self.topics[paper[0]] = []
+            self.topics[paper[0]].append(newPaper)
+
+            # split random discipline with prob pd
+            # if random.random() < probSplit:
+            #     commNodes = getCommNodes(network, random.choice(list(topics.keys())))
+            #     splitCommunity(network, commNodes)
+
+            # merge random discipline with prob pm
+            if random.random() < self.probMerge:
+                pass
+
+        self.initialPaper += (timeSteps - 1)
+        print(f'Authors: {self.network.nodes(data=True)}')
+        print(f'Papers: {self.papers}')
+        print(f'Topics: {self.topics}')
+        print(f'Initial Paper: {self.initialPaper}')
 
     def saveEvolutionWithPickle(self, fileName='evolution.env'):
         with open(fileName, 'wb') as outfile:
---
+++ main.py
@@ -0,0 +1,7 @@
+from Evolution import Evolution
+
+env = Evolution()
+
+env.evolve()
+
+env.evolve(timeSteps=30)
